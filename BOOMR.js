2f1bb5ddd17d4543f9e19839d3c425b56378d4b1
SkyN9ne <82126207+SkyN9ne@users.noreply.github.com>
2023-04-09 10:51:42 +0300

    Create BOOMR.js

---
+++ BOOMR.js
@@ -0,0 +1,147 @@
+function launchWebDirect() {
+    webdirect.renderWebDirect(
+        function () {
+            return document.getElementById("root");
+        }, {
+            clientContext: clientContext,
+            loggingContext: loggingContext,
+            preloadedLocalization: localizedStrings,
+            publicPath: "acquire/purchase"
+        }
+    );
+}
+
+
+ (window.BOOMR_mq = window.BOOMR_mq || []).push(["addVar", {
+    "rua.upush": "true",
+    "rua.cpush": "false",
+    "rua.upre": "false",
+    "rua.cpre": "false",
+    "rua.uprl": "false",
+    "rua.cprl": "false",
+    "rua.cprf": "false",
+    "rua.trans": "SJ-1828516d-5232-4e4e-9869-181601db7178",
+    "rua.cook": "true",
+    "rua.ims": "false",
+    "rua.ufprl": "false",
+    "rua.cfprl": "false",
+    "rua.isuxp": "false",
+    "rua.texp": "norulematch"
+}]);
+</script> <
+script > ! function (e) {
+    var n = "https://s.go-mpulse.net/boomerang/";
+    if ("False" == "True") e.BOOMR_config = e.BOOMR_config || {}, e.BOOMR_config.PageParams = e.BOOMR_config.PageParams || {}, e.BOOMR_config.PageParams.pci = !0, n = "https://s2.go-mpulse.net/boomerang/";
+    if (window.BOOMR_API_key = "NXXN5-4NEP7-RESXQ-U9RVR-NZ5S3", function () {
+            function e() {
+                if (!o) {
+                    var e = document.createElement("script");
+                    e.id = "boomr-scr-as", e.src = window.BOOMR.url, e.async = !0, i.parentNode.appendChild(e), o = !0
+                }
+            }
+
+            function t(e) {
+                o = !0;
+                var n, t, a, r, d = document,
+                    O = window;
+                if (window.BOOMR.snippetMethod = e ? "if" : "i", t = function (e, n) {
+                        var t = d.createElement("script");
+                        t.id = n || "boomr-if-as", t.src = window.BOOMR.url, BOOMR_lstart = (new Date).getTime(), e = e || d.body, e.appendChild(t)
+                    }, !window.addEventListener && window.attachEvent && navigator.userAgent.match(/MSIE [67]\./)) return window.BOOMR.snippetMethod = "s", void t(i.parentNode, "boomr-async");
+                a = document.createElement("IFRAME"), a.src = "about:blank", a.title = "", a.role = "presentation", a.loading = "eager", r = (a.frameElement || a).style, r.width = 0, r.height = 0, r.border = 0, r.display = "none", i.parentNode.appendChild(a);
+                try {
+                    O = a.contentWindow, d = O.document.open()
+                } catch (_) {
+                    n = document.domain, a.src = "javascript:var d=document.open();d.domain='" + n + "';void(0);", O = a.contentWindow, d = O.document.open()
+                }
+                if (n) d._boomrl = function () {
+                    this.domain = n, t()
+                }, d.write("<bo" + "dy onload='document._boomrl();'>");
+                else if (O._boomrl = function () {
+                        t()
+                    }, O.addEventListener) O.addEventListener("load", O._boomrl, !1);
+                else if (O.attachEvent) O.attachEvent("onload", O._boomrl);
+                d.close()
+            }
+
+            function a(e) {
+                window.BOOMR_onload = e && e.timeStamp || (new Date).getTime()
+            }
+            if (!window.BOOMR || !window.BOOMR.version && !window.BOOMR.snippetExecuted) {
+                window.BOOMR = window.BOOMR || {}, window.BOOMR.snippetStart = (new Date).getTime(), window.BOOMR.snippetExecuted = !0, window.BOOMR.snippetVersion = 12, window.BOOMR.url = n + "NXXN5-4NEP7-RESXQ-U9RVR-NZ5S3";
+                var i = document.currentScript || document.getElementsByTagName("script")[0],
+                    o = !1,
+                    r = document.createElement("link");
+                if (r.relList && "function" == typeof r.relList.supports && r.relList.supports("preload") && "as" in r) window.BOOMR.snippetMethod = "p", r.href = window.BOOMR.url, r.rel = "preload", r.as = "script", r.addEventListener("load", e), r.addEventListener("error", function () {
+                    t(!0)
+                }), setTimeout(function () {
+                    if (!o) t(!0)
+                }, 3e3), BOOMR_lstart = (new Date).getTime(), i.parentNode.appendChild(r);
+                else t(!1);
+                if (window.addEventListener) window.addEventListener("load", a, !1);
+                else if (window.attachEvent) window.attachEvent("onload", a)
+            }
+        }(), "".length > 0)
+        if (e && "performance" in e && e.performance && "function" == typeof e.performance.setResourceTimingBufferSize) e.performance.setResourceTimingBufferSize();
+    ! function () {
+        if (BOOMR = e.BOOMR || {}, BOOMR.plugins = BOOMR.plugins || {}, !BOOMR.plugins.AK) {
+            var n = "true" == "true" ? 1 : 0,
+                t = "cookiepresent",
+                a = "nxycc2icc6ghmzbsm74a-f-2489faee2-clientnsv4-s.akamaihd.net",
+                i = "false" == "true" ? 2 : 1,
+                o = {
+                    "ak.v": "34",
+                    "ak.cp": "1303372",
+                    "ak.ai": parseInt("780094", 10),
+                    "ak.ol": "0",
+                    "ak.cr": 36,
+                    "ak.ipv": 4,
+                    "ak.proto": "h2",
+                    "ak.rid": "109b7199",
+                    "ak.r": 15267,
+                    "ak.a2": n,
+                    "ak.m": "dsca",
+                    "ak.n": "essl",
+                    "ak.bpcip": "109.240.33.0",
+                    "ak.cport": 51649,
+                    "ak.gh": "95.101.133.143",
+                    "ak.quicv": "",
+                    "ak.tlsv": "tls1.3",
+                    "ak.0rtt": "",
+                    "ak.csrc": "-",
+                    "ak.acc": "",
+                    "ak.t": "1681025016",
+                    "ak.ak": "hOBiQwZUYzCg5VSAfCLimQ==T+uXD8jgvJAuGE3ytnyC5s7b6ep6CpfTq9/mMtdM01FOU4/W0PfgbaW4FAkVTB/IEIINE90UWwJJLn93biv3/L0WvTKBPOxYxkFZpU5ADKA2M7CtDzebYU8GmlLpTeq4fuT/Q/rdUUhxwr2OiSw9dTFYIyTUnp6s8uzfZaXXtkHAovooD3spJCMU0ovhIVDTYz36qy8RYEJ7pEqEDMFu7bcQ/wuveyasgwDU1JRr+94iiRv2nRAsA8SlkWK+ZwZdWelP6YRRA42KKdMsnbitFInBLeA0NuRYZhH1Ap01pZr3AeV6OdQTCQXKpx5r0ZBu7d/UDMKNUlg/V0CQXYLhZSv88OnWq350LAiFlsnu4lm/5bbeAZNHLrBMkc/n369Oal7AK7f88jsQ1cftKZI8BwnpaZIovTAD2LOWTcp8Bcs=",
+                    "ak.pv": "16",
+                    "ak.dpoabenc": "",
+                    "ak.tf": i
+                };
+            if ("" !== t) o["ak.ruds"] = t;
+            var r = {
+                i: !1,
+                av: function (n) {
+                    var t = "http.initiator";
+                    if (n && (!n[t] || "spa_hard" === n[t])) o["ak.feo"] = void 0 !== e.aFeoApplied ? 1 : 0, BOOMR.addVar(o)
+                },
+                rv: function () {
+                    var e = ["ak.bpcip", "ak.cport", "ak.cr", "ak.csrc", "ak.gh", "ak.ipv", "ak.m", "ak.n", "ak.ol", "ak.proto", "ak.quicv", "ak.tlsv", "ak.0rtt", "ak.r", "ak.acc", "ak.t", "ak.tf"];
+                    BOOMR.removeVar(e)
+                }
+            };
+            BOOMR.plugins.AK = {
+                akVars: o,
+                akDNSPreFetchDomain: a,
+                init: function () {
+                    if (!r.i) {
+                        var e = BOOMR.subscribe;
+                        e("before_beacon", r.av, null, null), e("onbeacon", r.rv, null, null), r.i = !0
+                    }
+                    return this
+                },
+                is_complete: function () {
+                    return !0
+                }
+            }
+        }
+    }()
+}(window);
